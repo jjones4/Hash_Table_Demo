3d7ebcf4d8ba144efd0bb1f90795060b48904b05
Alma <alma@alma-c-box.local>
2022-12-29 22:31:08 -0600

    Improve Creation of Hash Table

--- Hash_Table_Demo/Program.cs
+++ Hash_Table_Demo/Program.cs
@@ -4,124 +4,127 @@
     {
         static void Main(string[] args)
         {
-            // Hard-coded key/value pairs
-            // Insert into a hash table later
-            string[,] data = new string[,]
+            // Create data to put into hash table later
+            List<string[]> data = new List<string[]>();
+
+            data.Add(new string[] { "a", "1" });
+            data.Add(new string[] { "ab", "12" });
+            data.Add(new string[] { "abc", "123" });
+            data.Add(new string[] { "car", "3118" });
+            data.Add(new string[] { "bac", "213" });
+            data.Add(new string[] { "az", "126" });
+            data.Add(new string[] { "abf", "126" });
+            data.Add(new string[] { "cab", "312" });
+            data.Add(new string[] { "pee", "1655" });
+            data.Add(new string[] { "poop", "16151516" });
+            data.Add(new string[] { "try", "201825" });
+            data.Add(new string[] { "ah", "18" });
+            data.Add(new string[] { "cc", "33" });
+            data.Add(new string[] { "bb", "22" });
+            data.Add(new string[] { "dii", "499" });
+            data.Add(new string[] { "tag", "2017" });
+            data.Add(new string[] { "golf", "715126" });
+            data.Add(new string[] { "see", "1955" });
+            data.Add(new string[] { "cat", "3120" });
+            data.Add(new string[] { "water", "23120518" });
+
+            // dynamic array for the hash table
+            List<object> hashTable = new List<object>();
+
+            // Loop through initial data set
+            // Add key/value pairs to hash table
+            int arrayIndex = 0;
+            int maxIndex = -1;
+
+            foreach (string[] item in data)
             {
-                { "a", "1" },
-                { "ab", "12" },
-                { "abc", "123" }, // Collisions with "bac" and "cab"
-                { "car", "3118" }, // Colision with "dii"
-                { "bac", "213" }, // Collisions with "abc" and "cab"
-                { "az", "126" }, // Collision with "tag"
-                { "abf", "126" },
-                { "cab", "312" }, // Collisions with "abc" and "bac"
-                { "pee", "1655" },
-                { "poop", "16151516" },
-                { "try", "201825" },
-                { "ah", "18" },
-                { "cc", "33" },
-                { "bb", "22" },
-                { "dii", "499" }, // Collision with "car"
-                { "tag", "2017" }, // Collision with "az"
-                { "golf", "715126" },
-                { "see", "1955" },
-                { "cat", "3120" },
-                { "water", "23120518" }
-            };
+                // Calculate our "hash", i.e., our array index
+                arrayIndex = calculateArrayIndex(item[0]);
 
-            // Create hash table and initialize it
-
-            // Hash table is an array of 130 arrays each of 3 arrays
-            // with 2 strings in each of those arrays
-            // [  [  [s1,s2][s1,s2][s1,s2]  ]  [  [s1,s2][s1,s2][s1,s2]  ]  ...  [  [s1,s2][s1,s2][s1,s2]  ]  ]
-            //           1     2      3              1      2      3                   1      2      3
-            //    1                            2 ...                             130
-            // Note that 130 is the largest sum of 5 letters "zzzzz"
-            // with ascii values of z = 122 - 96 for each "z"
-            // We could have up to 130 for an index
-            string[,,] hashTable = new string[130, 3, 2];
-
-            for (int i = 0; i < 130; i++)
-            {
-                hashTable[i, 0, 0] = "-";
-                hashTable[i, 0, 1] = "-";
-                hashTable[i, 1, 0] = "-";
-                hashTable[i, 1, 1] = "-";
-                hashTable[i, 2, 0] = "-";
-                hashTable[i, 2, 1] = "-";
-            }
-
-            // Put the data from our data array into the hash table
-            // at the correct indeces (calculated by our calculateIndex
-            // method)
-            for (int i = 0; i < data.Length / 2; i++)
-            {
-                // Check for collisions
-
-                // Check to make sure the first array of the three isn't filled
-                if (hashTable[calculateIndex(data[i, 0]), 0, 0] == "-")
+                // If next key produces a hash value greater than any
+                // previous index
+                if (arrayIndex > maxIndex)
                 {
-                    hashTable[calculateIndex(data[i, 0]), 0, 0] = data[i, 0];
-                    hashTable[calculateIndex(data[i, 0]), 0, 1] = data[i, 1];
-                }
-                // Check to make sure the second array of the three isn't filled
-                else if (hashTable[calculateIndex(data[i, 0]), 1, 0] == "-")
-                {
-                    hashTable[calculateIndex(data[i, 0]), 1, 0] = data[i, 0];
-                    hashTable[calculateIndex(data[i, 0]), 1, 1] = data[i, 1];
+                    // Fill hash table with null values up to new max index
+                    for (int i = maxIndex + 1; i < arrayIndex; i++)
+                    {
+                        hashTable.Add(null);
+                    }
 
+                    hashTable.Add(item);
+
+                    maxIndex = arrayIndex;
                 }
-                // Put the data in the third array (we don't have more than 3
-                // collisions per index
                 else
                 {
-                    hashTable[calculateIndex(data[i, 0]), 2, 0] = data[i, 0];
-                    hashTable[calculateIndex(data[i, 0]), 2, 1] = data[i, 1];
+                    // If no data has been stored at this index
+                    if (hashTable[arrayIndex] == null)
+                    {
+                        hashTable[arrayIndex] = item;
+                    }
+                    else
+                    {
+                        // If there is just one string[] in this index
+                        if (hashTable[arrayIndex].GetType().IsArray)
+                        {
+                            LinkedList<string[]> tempList = new LinkedList<string[]>();
+                            tempList.AddLast((string[])hashTable[arrayIndex]);
+                            tempList.AddLast(item);
+                            hashTable[arrayIndex] = tempList;
+                        }
+                        // If there is already a linked list in this index
+                        else
+                        {
+                            ((LinkedList<string[]>)hashTable[arrayIndex]).AddLast(item);
+                        }
+                    }
                 }
             }
 
-            Console.WriteLine();
-
-            // Test some searching of the hash table by key from the original data set
-            // Print out some values retrieved by using the keys from the original data set
-            printValue(hashTable, "a"); // { "a", "1" }
-            printValue(hashTable, "abc"); // { "abc", "123" }
-            printValue(hashTable, "car"); // { "car", "3118" }
-            printValue(hashTable, "poop"); // { "poop", "16151516" }
-            printValue(hashTable, "dii"); // { "dii", "499" }
-            printValue(hashTable, "water"); // { "water", "23120518" }
-        }
-
-        static void printValue(string[,,] table, string key)
-        {
-            int index = calculateIndex(key);
-
-            // Search each of the three inner arrays
-            for (int i = 0; i < 3; i++)
+            // Print hash table to ensure accuracty
+            foreach (var item in hashTable)
             {
-                // Check for collisions and move to the next array if needed
-                // to find the key
-                if (table[index, i, 0] == key)
+                if (item == null)
                 {
-                    Console.WriteLine($"   The given key was: {key}. " +
-                        $"The associated value is: {table[index, i, 1]}.");
-
-                    break;
+                    continue;
                 }
+                else
+                {
+                    if (item.GetType().IsArray)
+                    {
+                        Console.Write($"Key {((string[])item)[0]}, Value {((string[])item)[1]}.");
+                    }
+                    else
+                    {
+                        Console.Write("Collision has occurred ...");
+                        Console.WriteLine(" Multiple key/value pairs stored in bucket.");
+
+                        foreach (string[] stringArray in (LinkedList<string[]>)item)
+                        {
+                            Console.Write($"Key {stringArray[0]}, Value {stringArray[1]}. ");
+                        }
+                    }
+                }
+
+                Console.WriteLine();
             }
         }
 
-        static int calculateIndex(string key)
+        /// <summary>
+        /// Calculates the index of our hash table by summing
+        /// the ASCII values of the letters (subtract 97 from
+        /// each letter in order to find the zero-based
+        /// position of the letter in the alphabet)
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static int calculateArrayIndex(string key)
         {
             int total = 0;
 
-            // ascii sum of lower case letters minus 96 to
-            // get a one-based count for our
-            // hash table array indeces
             foreach (char c in key)
             {
-                total += (int)(c) - 96;
+                total += (int)(c) - 97;
             }
 
             return total;
